2162b5971fc3554bed6ab20511588721e7a33b54
Valentin <piterans@warp.is>
2023-05-11 19:41:59 +0300

    settings and refactoring

---
+++ app/settings.py
@@ -0,0 +1,27 @@
+from functools import lru_cache
+
+from dotenv import load_dotenv
+from pydantic import BaseSettings
+
+load_dotenv(verbose=True)
+
+
+class Settings(BaseSettings):
+    db_user: str
+    db_password: str
+    db_port: int
+    db_host: str
+
+    class Config:
+        env_file = "../.env"
+        # env_file = find_dotenv(".env")
+        # env_file = os.path.join(os.getcwd(), '.env')
+        env_file_encoding = 'utf-8'
+
+
+@lru_cache()
+def get_settings():
+    return Settings()
+
+
+settings = get_settings()
